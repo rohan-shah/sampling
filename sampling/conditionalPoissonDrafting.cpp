6fff9dcbc87de94b1ebfa57d87ebf833655ae575
Rohan Shah <github@bookshah.com>
2016-06-09 00:06:06 +1000

    More work on the drafting method for conditional Poisson sampling. It seems to be working?

--- sampling/conditionalPoissonDrafting.cpp
+++ sampling/conditionalPoissonDrafting.cpp
@@ -49,6 +49,8 @@ namespace sampling
 		conditionalPoissonInclusionProbabilities(args);
 		args.inclusionProbabilities2.clear();
 		args.inclusionProbabilities2.insert(args.inclusionProbabilities2.begin(), inclusionProbabilities.begin(), inclusionProbabilities.end());
+		//They have to be rescaled first
+		for(int i = 0; i < (int)args.inclusionProbabilities2.size(); i++) args.inclusionProbabilities2[i] /= args.n - deterministicIndices;
 		//drafting procedure
 		std::vector<int> remaining;
 		remaining.reserve(nUnits);
@@ -58,6 +60,7 @@ namespace sampling
 		}
 		std::vector<double> accumulated;
 		accumulated.reserve(nUnits);
+		std::vector<int> equalProbabilityUnits;
 		boost::random::uniform_real_distribution<double> uniformDest;
 		for(int i = 0; i < (int)args.n - deterministicIndices; i++)
 		{
@@ -78,18 +81,41 @@ namespace sampling
 				{
 					selectedUnit = remaining[j];
 					selectedUnitInRemaining = j;
+					break;
 				}
 			}
 			if(selectedUnit == -1) throw std::runtime_error("Internal error");
+			indices.push_back(selectedUnit);
 			//Update the inclusion probabilities to reflect the sampled unit. See Rare Event Simulation Using Monte Carlo Methods, edited by Gerardo Rubino and Bruno Tuffin, p183. 
 			args.inclusionProbabilities3.resize(nUnits);
-			std::swap(remaining.back(), remaining[selectedUnitInRemaining]);
-			remaining.pop_back();
-			if(args.n - deterministicIndices - i - 1 == 0) throw std::runtime_error("Internal error");
+			if(args.n - deterministicIndices - i - 1 == 0) 
+			{
+				break;
+			}
+			//Things get very messy in the case that there are multiple units with the same weight. See Sampling Algorithms by Yves Tille, pp 102. 
+			mpfr_class sumNotEqual = 1;
+			equalProbabilityUnits.clear();
 			for(int j = 0; j < (int)remaining.size(); j++)
 			{
-				args.inclusionProbabilities3[remaining[j]] = (args.expExponentialParameters[selectedUnit] * args.inclusionProbabilities2[remaining[j]] - args.expExponentialParameters[remaining[j]] * args.inclusionProbabilities2[selectedUnit]) / ((args.n - deterministicIndices - i - 1) * (args.expExponentialParameters[selectedUnit] - args.expExponentialParameters[remaining[j]]) * args.inclusionProbabilities2[selectedUnit]);
+				if(j != selectedUnitInRemaining)
+				{
+					if(args.expExponentialParameters[selectedUnit] == args.expExponentialParameters[remaining[j]])
+					{
+						equalProbabilityUnits.push_back(remaining[j]);
+					}
+					else
+					{
+						args.inclusionProbabilities3[remaining[j]] = (args.expExponentialParameters[selectedUnit] * args.inclusionProbabilities2[remaining[j]] - args.expExponentialParameters[remaining[j]] * args.inclusionProbabilities2[selectedUnit]) / ((args.n - deterministicIndices - i - 1) * (args.expExponentialParameters[selectedUnit] - args.expExponentialParameters[remaining[j]]) * args.inclusionProbabilities2[selectedUnit]);
+						sumNotEqual -= args.inclusionProbabilities3[remaining[j]];
+					}
+				}
 			}
+			for(std::vector<int>::iterator j = equalProbabilityUnits.begin(); j != equalProbabilityUnits.end(); j++)
+			{
+				args.inclusionProbabilities3[*j] = sumNotEqual / equalProbabilityUnits.size();
+			}
+			std::swap(remaining.back(), remaining[selectedUnitInRemaining]);
+			remaining.pop_back();
 			args.inclusionProbabilities3.swap(args.inclusionProbabilities2);
 		}
 	}
---
+++ samplingTests/conditionalPoissonDrafting.cpp
@@ -0,0 +1,66 @@
+#include <boost/test/unit_test.hpp>
+#include "conditionalPoissonDrafting.h"
+BOOST_AUTO_TEST_CASE(conditionalPoissonDrafting1, * boost::unit_test::tolerance(0.00001))
+{
+	sampling::conditionalPoissonDraftingArgs args;
+	std::vector<int> indices;
+	std::vector<sampling::mpfr_class> inclusionProbabilities, weights, rescaledWeights;
+	args.indices = &indices;
+	args.inclusionProbabilities = &inclusionProbabilities;
+	args.weights = &weights;
+	args.rescaledWeights = &rescaledWeights;
+	args.calculateInclusionProbabilities = true;
+
+	boost::mt19937 randomSource;
+	randomSource.seed(1);
+
+	weights.push_back(1);
+	weights.push_back(2);
+	weights.push_back(3);
+	args.n = 2;
+	int table[2];
+	table[0] = table[1] = 0;
+	for(int i = 0; i < 5000; i++)
+	{
+		conditionalPoissonDrafting(args, randomSource);
+		std::sort(indices.begin(), indices.end());
+		BOOST_TEST((int)indices.size() == 2);
+		BOOST_TEST(indices[1] == 2);
+		table[indices[0]]++;
+	}
+	BOOST_TEST(table[0]/5000.0 == 0.2, boost::test_tools::tolerance(0.02));
+	BOOST_TEST(table[1]/5000.0 == 0.8, boost::test_tools::tolerance(0.02));
+}
+BOOST_AUTO_TEST_CASE(conditionalPoissonDrafting2, * boost::unit_test::tolerance(0.00001))
+{
+	sampling::conditionalPoissonDraftingArgs args;
+	std::vector<int> indices;
+	std::vector<sampling::mpfr_class> inclusionProbabilities, weights, rescaledWeights;
+	args.indices = &indices;
+	args.inclusionProbabilities = &inclusionProbabilities;
+	args.weights = &weights;
+	args.rescaledWeights = &rescaledWeights;
+	args.calculateInclusionProbabilities = true;
+
+	boost::mt19937 randomSource;
+	randomSource.seed(1);
+
+	weights.push_back(1);
+	weights.push_back(2);
+	weights.push_back(2);
+	args.n = 2;
+	int table[3];
+	table[0] = table[1] = table[2] = 0;
+	for(int i = 0; i < 5000; i++)
+	{
+		conditionalPoissonDrafting(args, randomSource);
+		std::sort(indices.begin(), indices.end());
+		BOOST_TEST((int)indices.size() == 2);
+		if(indices[0] == 0 && indices[1] == 1) table[0]++;
+		if(indices[0] == 0 && indices[1] == 2) table[1]++;
+		if(indices[0] == 1 && indices[1] == 2) table[2]++;
+	}
+	BOOST_TEST(table[0]/5000.0 == 0.125, boost::test_tools::tolerance(0.02));
+	BOOST_TEST(table[1]/5000.0 == 0.125, boost::test_tools::tolerance(0.02));
+	BOOST_TEST(table[2]/5000.0 == 0.75, boost::test_tools::tolerance(0.02));
+}
